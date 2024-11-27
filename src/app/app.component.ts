01418e74929fdeabd554c2f6c9589ec333b64034
Tania Rosales <tania19rosales@gmail.com>
2024-11-27 05:17:18 -0600

    cambios para que login funcione

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import firebase from 'firebase/compat/app';
+import { LoginService } from './componentes/login/login.service';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
+  providers: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
+  constructor (private loginService: LoginService){}
+
+  ngOnInit(): void {
+      firebase.initializeApp({
+        apiKey: "AIzaSyBgFJ2arW--1MHQgchCTP1LaTUORWA574o",
+        authDomain: "iburger-6f44d.firebaseapp.com"
+      });
+  }
+
+  estaLogueado(){
+    return this.loginService.estaLogueado();
+  }
+
+  logout(){
+    this.loginService.logout();
+  }
   
 }
