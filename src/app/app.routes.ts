e5333296246336eb7c78c67517be078fce6b4de2
Tania Rosales <tania19rosales@gmail.com>
2024-11-27 05:17:42 -0600

    nuevas rutas de registro de usuario, login y protección con guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,15 +7,20 @@ import { OrdenesPadreComponent } from './componentes/ordenes-padre/ordenes-padre
 import { ActualizaComboComponent } from './componentes/actualiza-combo/actualiza-combo.component';
 import { ActualizaOrdenComponent } from './componentes/actualiza-orden/actualiza-orden.component';
 import { ActualizaEmpleadoComponent } from './componentes/actualiza-empleado/actualiza-empleado.component';
+import { LoginComponent } from './componentes/login/login.component';
+import { LoginGuardian } from './componentes/login/login-guardian';
+import { RegistroComponent } from './componentes/registro/registro.component';
 
 export const routes: Routes = [
-  {path: "", component: HomeComponentComponent},
-  {path: "ordenes", component: OrdenesPadreComponent},
-  {path: "combos", component: CombosComponentComponent},
-  {path: "empleados", component: EmpleadosComponentComponent},
-  {path: "actualizaOrden/:id", component: ActualizaOrdenComponent},
-  {path: "ordenes/actualizaOrden/:id", component: ActualizaOrdenComponent},
-  {path: "combos/actualizaCombo/:id", component: ActualizaComboComponent},
-  {path: "empleados/actualizaEmpleado/:id", component: ActualizaEmpleadoComponent},
+  {path: "", component: HomeComponentComponent, canActivate:[LoginGuardian]},
+  {path: "ordenes", component: OrdenesPadreComponent, canActivate:[LoginGuardian]},
+  {path: "combos", component: CombosComponentComponent, canActivate:[LoginGuardian]},
+  {path: "empleados", component: EmpleadosComponentComponent, canActivate:[LoginGuardian]},
+  {path: "actualizaOrden/:id", component: ActualizaOrdenComponent, canActivate:[LoginGuardian]},
+  {path: "ordenes/actualizaOrden/:id", component: ActualizaOrdenComponent, canActivate:[LoginGuardian]},
+  {path: "combos/actualizaCombo/:id", component: ActualizaComboComponent, canActivate:[LoginGuardian]},
+  {path: "empleados/actualizaEmpleado/:id", component: ActualizaEmpleadoComponent, canActivate:[LoginGuardian]},
+  {path: "login", component: LoginComponent},
+  {path: "registro", component: RegistroComponent},
   {path: "**", component: ErrorPersonalizadoComponent}
 ];
